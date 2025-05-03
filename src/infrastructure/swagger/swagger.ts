f1c8f9916c5beb7ab63eb1d127dc106534604a32
jean palomino <118295259+jean-heyller@users.noreply.github.com>
2025-05-03 01:12:20 -0500

    feature/Sprint2/módulo-vehículos: Se agrega la configuración de swagger para la instancia en develop

--- src/infrastructure/swagger/swagger.ts
+++ src/infrastructure/swagger/swagger.ts
@@ -1,5 +1,7 @@
 import swaggerJsDoc from "swagger-jsdoc";
 
+const isLocal = process.env.IS_OFFLINE === "true"; // Detectar si estás ejecutando localmente
+
 const swaggerOptions = {
   definition: {
     openapi: "3.0.0",
@@ -10,7 +12,9 @@ const swaggerOptions = {
     },
     servers: [
       {
-        url: "http://localhost:3001", // Cambia esto si usas un entorno de desarrollo en AWS
+        url: isLocal
+          ? "http://localhost:3000" // URL para desarrollo local
+          : `https://${process.env.API_GATEWAY_ID}.execute-api.${process.env.AWS_REGION}.amazonaws.com/${process.env.STAGE}`, // URL para AWS
       },
     ],
   },
