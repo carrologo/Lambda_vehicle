ef326358e42bbb0ed4654aed6756484f4dd3001e
jean palomino <118295259+jean-heyller@users.noreply.github.com>
2025-05-06 16:47:06 -0500

    feature/Sprint2/módulo-vehículos: Se deja opcional el campo de images

--- src/application/use-cases/CreateVehicle.ts
+++ src/application/use-cases/CreateVehicle.ts
@@ -2,32 +2,33 @@ import { Vehicle } from "../../domain/entities/Vehicle";
 import { IUploadImagesRepository } from "../../domain/repositories/IUploadImagesRepository";
 import { VehicleRepository } from "../../infrastructure/database/SupabaseVehicleRepository";
 export class CreateVehicle {
-  constructor(private vehicleRepository: VehicleRepository,
+  constructor(
+    private vehicleRepository: VehicleRepository,
     private uploadImagesRepository: IUploadImagesRepository
   ) {}
 
   async execute(vehicle: Vehicle): Promise<string> {
     try {
-      const urlImages = await this.uploadImagesRepository.uploadImages(
-        vehicle.images || [],
-        `vehicles/${vehicle.brand}/${vehicle.line}/${vehicle.model}`
-      );
+      if (vehicle.images && vehicle.images.length > 0) {
+        const urlImages = await this.uploadImagesRepository.uploadImages(
+          vehicle.images || [],
+          `vehicles/${vehicle.brand}/${vehicle.line}/${vehicle.model}`
+        );
 
-      if (urlImages.length === 0) {
-        throw new Error("No se pudieron subir las imágenes.");
+        if (urlImages.length === 0) {
+          throw new Error("No se pudieron subir las imágenes.");
+        } else {
+          vehicle.url_images = urlImages;
+          delete vehicle.images; // Eliminar la propiedad images después de subirlas
+          await this.vehicleRepository.save(vehicle);
+        }
       }
-
-   
-      else {
-        vehicle.url_images = urlImages;
-        delete vehicle.images; // Eliminar la propiedad images después de subirlas
-        await this.vehicleRepository.save(vehicle);
-      }
+      await this.vehicleRepository.save(vehicle);
 
       return "El vehículo fue ingresado correctamente.";
     } catch (error) {
       throw new Error(
-        `Failed to create client: ${
+        `Failed to creat Vehicle: ${
           error instanceof Error ? error.message : "Unknown error"
         }`
       );
