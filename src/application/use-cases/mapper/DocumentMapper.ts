e9745578cf903010ec521d8307d6b0c5355c4e90
jean palomino <118295259+jean-heyller@users.noreply.github.com>
2025-07-16 16:30:03 -0500

    feature/Sprint4/módulo-vehículo: Se ajusta la inyección de dependencias

--- src/application/use-cases/mapper/DocumentMapper.ts
+++ src/application/use-cases/mapper/DocumentMapper.ts
@@ -2,8 +2,7 @@ export class DocumentMapper {
   static toDatabase(data: any): any {
     return {
       expiration_date: data.expirationDate ? new Date(data.expirationDate) : null,
-      document_type_id: data.documentTypeId,
-      category: data.category
+      document_type_id: data.documentTypeId
     };
   }
 }
