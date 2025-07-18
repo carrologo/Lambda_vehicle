2e44c1ef35dcbd83328cb48e74ab8b214789bcd8
blessed0314 <christian.daniel999@gmail.com>
2025-07-18 16:27:24 -0500

    feature/sprint3/módulo-vehículo: fix cors

--- src/infrastructure/lambdas/CorsResponse.ts
+++ src/infrastructure/lambdas/CorsResponse.ts
@@ -3,6 +3,7 @@ export const corsResponse = (statusCode: number, body: any) => {
       statusCode,
       headers: {
         "Access-Control-Allow-Origin": "https://develcarrologo.netlify.app",
+        "Vary": "Origin",
         "Access-Control-Allow-Headers":
           "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
         "Access-Control-Allow-Methods": "OPTIONS,POST,GET,PATCH,DELETE",
